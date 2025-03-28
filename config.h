5bee8a9ca630865ec52738c9c1e2310d31e424d6
agus <33077305+agusc01@users.noreply.github.com>
2025-03-28 18:17:13 -0300

    Change keys (zoom with ctrlshift N & M)

--- config.h
+++ config.h
@@ -185,7 +185,7 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define CTRL_SHIFT (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
@@ -193,14 +193,13 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ CTRL_SHIFT,           XK_M,           zoom,           {.f = +1} },
+	{ CTRL_SHIFT,           XK_N,           zoom,           {.f = -1} },
+	{ CTRL_SHIFT,           XK_Home,        zoomreset,      {.f =  0} },
+	{ CTRL_SHIFT,           XK_C,           clipcopy,       {.i =  0} },
+	{ CTRL_SHIFT,           XK_V,           clippaste,      {.i =  0} },
+	{ CTRL_SHIFT,           XK_Y,           selpaste,       {.i =  0} },
+	{ CTRL_SHIFT,           XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
 /*
